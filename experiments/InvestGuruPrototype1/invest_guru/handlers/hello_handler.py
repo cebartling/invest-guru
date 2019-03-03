0136199bc41c4565ceb360aa5e74c28a7ea3768c
Christopher Bartling <chris.bartling@gmail.com>
2019-03-03 15:01:46 -0600

    AppSync API now calling Lambda function!

--- experiments/InvestGuruPrototype1/invest_guru/handlers/hello_handler.py
+++ experiments/InvestGuruPrototype1/invest_guru/handlers/hello_handler.py
@@ -1,24 +1,19 @@
-import json
-
-
 def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    # body = {
+    #     "message": "Go Serverless v1.0! Your function executed successfully!",
+    #     "input": event
+    # }
+    #
+    # response = {
+    #     "statusCode": 200,
+    #     "body": json.dumps(body)
+    # }
+    #
+    # return response
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
+    name = event['name']
 
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "content": "Hello {} from Lambda function".format(name),
         "event": event
     }
-    """
