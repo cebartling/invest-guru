2adf7d10a839eb2a107b9a365c797bdc16729551
Christopher Bartling <chris.bartling@gmail.com>
2019-03-09 12:15:10 -0600

    Refactoring.

--- experiments/InvestGuruPrototype1/invest_guru/alpha_vantage/simple_moving_average.py
+++ experiments/InvestGuruPrototype1/invest_guru/alpha_vantage/simple_moving_average.py
@@ -2,24 +2,29 @@ import os
 
 import requests
 
-TECHNICAL_ANALYSIS_SMA_KEY = "Technical Analysis: SMA"
+TECHNICAL_ANALYSIS_SMA_KEY = 'Technical Analysis: SMA'
 SMA_KEY = 'SMA'
 
 
-def simple_moving_average(symbol, time_period=50):
-    function_param = "function=SMA"
-    symbol_param = "symbol={}".format(symbol)
-    interval_param = "interval=daily"
-    time_period_param = "time_period={}".format(time_period)
-    series_type_param = "series_type=close"
+def _prepare_api_url(symbol, time_period):
+    function_param = 'function=SMA'
+    symbol_param = 'symbol={}'.format(symbol)
+    interval_param = 'interval=daily'
+    time_period_param = 'time_period={}'.format(time_period)
+    series_type_param = 'series_type=close'
     alpha_vantage_api_key = os.environ['alphaVantageApiKey']
-    api_key_param = "apikey={}".format(alpha_vantage_api_key)
-    api_url = "https://www.alphavantage.co/query?{}&{}&{}&{}&{}&{}".format(function_param,
+    api_key_param = 'apikey={}'.format(alpha_vantage_api_key)
+    api_url = 'https://www.alphavantage.co/query?{}&{}&{}&{}&{}&{}'.format(function_param,
                                                                            symbol_param,
                                                                            interval_param,
                                                                            time_period_param,
                                                                            series_type_param,
                                                                            api_key_param)
+    return api_url
+
+
+def simple_moving_average(symbol, time_period=50):
+    api_url = _prepare_api_url(symbol, time_period)
     result = dict()
     response = requests.get(api_url)
     if response.status_code == requests.codes.ok:
