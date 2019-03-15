69c8f9cfc1ba437449b7c0128ae00dd5d46de3fa
Christopher Bartling <chris.bartling@gmail.com>
2019-03-15 11:52:56 -0500

    Removed print statement.

--- experiments/InvestGuruPrototype1/invest_guru/alpha_vantage/simple_moving_average_test.py
+++ experiments/InvestGuruPrototype1/invest_guru/alpha_vantage/simple_moving_average_test.py
@@ -7,4 +7,3 @@ from invest_guru.alpha_vantage.simple_moving_average import simple_moving_averag
 def test__simple_moving_average__success():
     result = simple_moving_average('MSFT', time_period=50)
     assert result is not None
-    print(result)
