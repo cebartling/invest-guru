a0a2d355fa3ed1d061db8788ff5a4804822b6b6f
Christopher Bartling <chris.bartling@gmail.com>
2019-03-27 17:23:49 -0500

    Moving app styles to SCSS file.

--- experiments/InvestGuruPrototype1/invest-guru-prototype1-vue/src/main.js
+++ experiments/InvestGuruPrototype1/invest-guru-prototype1-vue/src/main.js
@@ -3,8 +3,11 @@ import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
 import apolloProvider from "@/appsync";
+import BootstrapVue from "bootstrap-vue";
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
 
 new Vue({
   router,
