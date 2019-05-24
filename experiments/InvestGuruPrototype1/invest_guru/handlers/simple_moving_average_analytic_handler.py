d824e58e810a68ed11ac8746d92d47b0a0844597
Christopher Bartling <chris.bartling@gmail.com>
2019-05-23 23:04:18 -0500

    Adding caching values in Redis.

--- experiments/InvestGuruPrototype1/invest_guru/handlers/simple_moving_average_analytic_handler.py
+++ experiments/InvestGuruPrototype1/invest_guru/handlers/simple_moving_average_analytic_handler.py
@@ -1,11 +1,23 @@
 from invest_guru.alpha_vantage.simple_moving_average import simple_moving_average
+import redis
 
 
 def handle(event, context):
     symbol = event['symbol']
 
-    sma_50_days = simple_moving_average(symbol=symbol, time_period=50)
-    sma_300_days = simple_moving_average(symbol=symbol, time_period=300)
+    redis_client = redis.StrictRedis(host='<your ElastiCache endpoint DNS>', port=6379, db=0)
+    company_info = redis_client.get(symbol)
+    if company_info is None:
+        sma_50_days = simple_moving_average(symbol=symbol, time_period=50)
+        sma_300_days = simple_moving_average(symbol=symbol, time_period=300)
+        company_info = {
+            'sma_50_days': sma_50_days,
+            'sma_300_days': sma_300_days
+        }
+        redis_client.set(symbol, company_info)
+    else:
+        sma_50_days = company_info['sma_50_days']
+        sma_300_days = company_info['sma_300_days']
 
     result = {
         "company": {
