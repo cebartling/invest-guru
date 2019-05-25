b51ed543709f890c72b193c843f19a64f2012bdf
Christopher Bartling <chris.bartling@gmail.com>
2019-05-25 16:48:49 -0500

    Checkpoint.

--- experiments/InvestGuruPrototype1/invest_guru/handlers/simple_moving_average_analytic_handler.py
+++ experiments/InvestGuruPrototype1/invest_guru/handlers/simple_moving_average_analytic_handler.py
@@ -1,23 +1,32 @@
+from redis import StrictRedis
+
 from invest_guru.alpha_vantage.simple_moving_average import simple_moving_average
-import redis
 
 
 def handle(event, context):
     symbol = event['symbol']
 
-    redis_client = redis.StrictRedis(host='<your ElastiCache endpoint DNS>', port=6379, db=0)
-    company_info = redis_client.get(symbol)
-    if company_info is None:
-        sma_50_days = simple_moving_average(symbol=symbol, time_period=50)
-        sma_300_days = simple_moving_average(symbol=symbol, time_period=300)
-        company_info = {
-            'sma_50_days': sma_50_days,
-            'sma_300_days': sma_300_days
-        }
-        redis_client.set(symbol, company_info)
-    else:
-        sma_50_days = company_info['sma_50_days']
-        sma_300_days = company_info['sma_300_days']
+    try:
+        redis_client = StrictRedis(host='redis-cluster.lr8qpd.0001.use1.cache.amazonaws.com', port=6379, db=0)
+        company_info = redis_client.get(symbol)
+    except Exception as e:
+        print(e)
+
+
+    # if company_info is None:
+    #     sma_50_days = simple_moving_average(symbol=symbol, time_period=50)
+    #     sma_300_days = simple_moving_average(symbol=symbol, time_period=300)
+    #     company_info = {
+    #         'sma_50_days': sma_50_days,
+    #         'sma_300_days': sma_300_days
+    #     }
+    #     redis_client.set(symbol, company_info)
+    # else:
+    #     sma_50_days = company_info['sma_50_days']
+    #     sma_300_days = company_info['sma_300_days']
+
+    sma_50_days = simple_moving_average(symbol=symbol, time_period=50)
+    sma_300_days = simple_moving_average(symbol=symbol, time_period=300)
 
     result = {
         "company": {
