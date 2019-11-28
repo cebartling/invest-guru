2214f471ef7388f041efa7b64a9a28fd46f87376
Christopher Bartling <chris.bartling@gmail.com>
2019-11-27 20:23:00 -0600

    Initial revision.

---
+++ invest-guru-spring-boot/src/main/java/com/pintailconsultingllc/investguru/InvestGuruApplication.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.investguru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestGuruApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestGuruApplication.class, args);
+    }
+
+}
---
+++ invest-guru-spring-boot/src/test/java/com/pintailconsultingllc/investguru/InvestGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pintailconsultingllc.investguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestGuruApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
