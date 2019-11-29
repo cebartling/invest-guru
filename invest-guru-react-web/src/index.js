942e830e39843736f88109ab01794f2647799fb8
Christopher Bartling <chris.bartling@gmail.com>
2019-11-29 00:22:31 -0600

    Adding ApolloClient 3.0 BETA.

--- invest-guru-react-web/src/index.js
+++ invest-guru-react-web/src/index.js
@@ -3,8 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: new HttpLink({
+        uri: 'https://48p1r2roz4.sse.codesandbox.io',
+    })
+});
+
+const ApolloApp = () => (
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+);
+
+ReactDOM.render(<ApolloApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
