22b382b6fcf5246aed9e49a47ee7ab83149ee2f6
Christopher Bartling <chris.bartling@gmail.com>
2019-11-30 17:20:04 -0600

    Adding routing animations to the mix.

--- invest-guru-react-web/src/AppRouter.js
+++ invest-guru-react-web/src/AppRouter.js
@@ -2,17 +2,26 @@ import React from 'react';
 import {Route, Switch} from "react-router-dom";
 import FrontDoorView from "./views/FrontDoorView";
 import DashboardView from "./views/DashboardView";
+import {CSSTransitionGroup} from "react-transition-group";
+import { useLocation} from "react-router";
 
 const AppRouter = () => {
+    const location = useLocation();
+
     return (
-        <Switch>
-            <Route path="/dashboard">
-                <DashboardView/>
-            </Route>
-            <Route path="/">
-                <FrontDoorView/>
-            </Route>
-        </Switch>
+        <CSSTransitionGroup
+            transitionName="view"
+            transitionEnterTimeout={500}
+            transitionLeaveTimeout={300}>
+            <Switch key={location.key} location={location}>
+                <Route path="/dashboard">
+                    <DashboardView/>
+                </Route>
+                <Route path="/">
+                    <FrontDoorView/>
+                </Route>
+            </Switch>
+        </CSSTransitionGroup>
     );
 };
 
